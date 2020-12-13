8e86e138a7e757e9bd30ccebce5d5de5f6a166f5
Waqar Ahmed <waqar.ahmed@walmart.com>
2020-12-13 16:19:52 +0500

    hello world

---
+++ app/src/main/java/com/waahm7/circlecidemo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.waahm7.circlecidemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
